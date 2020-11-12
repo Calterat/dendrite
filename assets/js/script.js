af19b8b06dfeb7ea000be636a3a8e50192c4a3a7
JGil <calterat@gmail.com>
2020-11-12 10:41:23 -0600

    pseudo code entered

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// API fetch for random facts and display cards using chosen CSS CDN in HTML whiteboard div
+
+// API query for additional info based on the randoms selected for the day and only those dragged to column 1/ method 1.
+
+// moment.js or other time js cdn for color coding the time bar in the HTML
+
+// make the columns drag and drop friendly, and make columns unlock at certain times of the day
+
+// create quiz for modal from API requests
+
+// display in modals the method procedures for each time an item is dragged to its method column
+
+// display in modal the quiz for the last step
+
+
+
