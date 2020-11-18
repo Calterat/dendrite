30c1557fb13b481adbcf98db0a299387554008ae
Calterat <Calterat@gmail.com>
2020-11-17 21:15:21 -0600

    added sortable function to move cards around on page. still buggy

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,39 @@ const populateCard = (type, thing) => {
     whiteboardEl.append(cardDivEl);
 }
 
+$(".method-container").sortable({
+    connectWith: $(".method-container"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    receive: function(event) {
+        // let re = / /gi;
+        // let query = event.target.childNodes[1].childNodes[1].innerText.replace(re, '+');
+        // let modalNum = event.target.id;
+        // let modalQuery = $(`#modalLink${modalNum}`);
+        // let modalLink = $('<a>').attr('href', `https://www.google.com/search?q=${query}`).attr('target', '_blank')
+        // modalLink.html(`Click here to see your google search on ${event.target.childNodes[1].childNodes[1].innerText}`);
+        // modalQuery.append(modalLink);
+        // $(`#modal-method${modalNum}`).addClass('active');
+    },
+    update: function(event) {
+        let tempArr = [];
+        $(this).children().each(function(){
+            let randoText = $(this).find('h3').text().trim();
+            let randoTitle = $(this).find('h4').text().trim();
+            tempArr.push({
+                randoFrom: randoText,
+                rando: randoTitle
+            })
+        });
+        let arrName = $(this).attr("id");
+        // if (arrName !== 'whiteboard') {
+        //     methodPlacement[arrName] = tempArr;
+        //     saveTasks();
+        // }
+      }
+  });
+
 // pulls random word and then spits it to it's card generator
 const randomWordFetch = () => {
     let wordsApiUrl = 'https://wordsapiv1.p.rapidapi.com/words/?random=true';
@@ -28,7 +61,7 @@ const randomWordFetch = () => {
                 return response.json();
             } else {
                 // alert(`ERROR: ${response.statusText}`);
-                populateWordCard('placeholder');
+                populateCard('placeholder');
             }
         })
         .then(response => {
